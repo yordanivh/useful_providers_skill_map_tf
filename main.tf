0350786ce4c81bd34e02e06a56949ca54168eddd
Yordan Halachev <yordan.i.halachev@gmail.com>
2019-11-20 17:31:18 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,32 +1,5 @@
 variable "animal" {}
 
-provider "aws" {
-	region = "us-east-2"
-}
-
-resource "aws_instance" "example" {
-	ami	      ="ami-0d5d9d301c853a04a"
-	instance_type ="t2.micro"
-	vpc_security_group_ids=[aws_security_group.instance.id]
-
-	user_data = <<-EOF
-		#!bin/bash
-		echo "Hello Alvaro" > index.html
-		nohup busybox httpd -f -p 8080 &
-		EOF
-}
-
-
-resource "aws_security_group" "instance" {
-	name = "terraform-example-instance2"
-	ingress {
-	  from_port   = 8080
-	  to_port     = 8080
-	  protocol    = "tcp"
-	  cidr_blocks = ["0.0.0.0/0"]
-	}
-}
-
 resource "random_id" "server" {
   #keepers = {
    # ami_id = "${var.ami_id}"  
